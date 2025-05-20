7574251b20242367a316798f0fb72ff01a12781c
Walker Strauss <161752631+walkerstrauss@users.noreply.github.com>
2025-05-19 23:58:57 -0400

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,13 @@
+// secrets.h
+
+/** Wi–Fi Credentials */
+const char* ssid = "YOUR_SSID"; // Your Wi-Fi SSID
+const char* password = "YOUR_PASSWORD"; // Your Wi-Fi Password
+
+/** API Keys */
+const char GoogleMapsWeatherApiKey = "YOUR_GOOGLE_MAPS_API_KEY" // Your Google Maps Weather API Key 
+const char* OpenAIApiKey = "YOUR_OPEN_AI_API_KEY"// Your OpenAI API Key
+
+/** Weather variables */
+const char* lat = "00.0000"; // Your location's latitude
+const char* lon = "00.0000"; // Your location's longitude
